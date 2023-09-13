f6c7e0377143536a8d8b3bb70d3c68c3d9ab9e4a
marchese89 <marchese89@hotmail.com>
2023-09-13 15:28:55 +0200

    first commit

---
+++ script/index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("create-task");
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  const task = document.getElementById("task");
+  if (task.value === "") {
+    return;
+  }
+
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("task-div");
+  taskDiv.innerHTML = `
+  <label>${task.value}</label><button>ELIMINA</button>
+  `;
+  document.getElementById("task-list").appendChild(taskDiv);
+  task.value = "";
+
+  taskDiv.querySelector("label").addEventListener("click", function (event) {
+    event.target.style.textDecoration = "line-through";
+  });
+
+  taskDiv.querySelector("button").addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+});
